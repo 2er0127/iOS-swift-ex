080609349b69bdb8cc41a66238905fbc8485dabb
zero <zero@zero.local>
2024-04-13 15:03:18 +0900

    feat: swift base init

---
+++ iOS-swift-ex/iOS_swift_exApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_swift_exApp.swift
+//  iOS-swift-ex
+//
+//  Created by zero on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_swift_exApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
